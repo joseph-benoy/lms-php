244e30814e6843c094977990d5e64d1762870d7a
joseph-benoy <josephbenoy03@gmail.com>
2021-04-26 23:20:31 +0530

    changes login.js for user and admin

--- user_auth.php
+++ user_auth.php
@@ -2,11 +2,11 @@
     require "strict.php";
     require "db.php";
     if(isset($_POST['email'])&&isset($_POST['password'])){
-        echo $db->check_password($_POST['email'],$_POST['password']);
+        echo $db->check_password($_POST['email'],$_POST['password'],false);
     }
     else{
         if(isset($_POST['email'])){
-            echo $db->check_email($_POST['email']);
+            echo $db->check_email($_POST['email'],false);
         }
     }
 ?>
