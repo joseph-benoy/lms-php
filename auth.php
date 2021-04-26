8a5dc3570be7f32ee01e63076d76f9a02c48f4c0
joseph-benoy <josephbenoy03@gmail.com>
2021-04-26 19:24:08 +0530

    added which value is to be checked

--- auth.php
+++ auth.php
@@ -1,5 +1,14 @@
 <?php declare(strict_types=1);
     require "db.php";
     $db = new DB("localhost","joseph","3057","lms_php");
-    echo $db->check_email($_POST['email'],true);
+    if(isset($_POST['email'])&&isset($_POST['password'])){
+        echo "both email and password!";
+    }
+    elseif(isset($_POST['email'])){
+        echo "only email\n";
+        echo $db->check_email($_POST['email'],true);
+    }
+    else{
+        echo "password!";
+    }
 ?>
