524fe09831b9c91bc0e5352f9d4b07da6f43d3e7
joseph-benoy <josephbenoy03@gmail.com>
2021-04-28 11:10:04 +0530

    tabs handling finfished adn side nav configed

--- admin/js/dashboard_ui.js
+++ admin/js/dashboard_ui.js
@@ -4,7 +4,28 @@ function closeMobMenu(){
 function openMobMenu(){
     $(`#mob_menu`).show();
 }
+function closeTabExcept(tab_name){
+    $(`#tab_containter`).children().not(`#${tab_name}`).hide();
+    $(`.side_nav_active`).removeClass(`side_nav_active`);
+}
+function openTab(event){
+    let tab_name = event.data.tab_name;
+    $(`#${tab_name}`).show();
+    closeTabExcept(tab_name);
+    $(this).addClass(`side_nav_active`);
+}
+
 $(document).ready(function(){
     $(`#navIcon`).click(openMobMenu);
     $(`#menu-close-icon`).click(closeMobMenu);
+//tab handling
+    $(`#home_btn`).click({tab_name:"home_tab"},openTab);
+    $(`#books_btn`).click({tab_name:"books_tab"},openTab);
+    $(`#members_btn`).click({tab_name:"members_tab"},openTab);
+    $(`#messages_btn`).click({tab_name:"messages_tab"},openTab);
+    $(`#requests_btn`).click({tab_name:"requests_tab"},openTab);
+    $(`#messages_btn`).click({tab_name:"messages_tab"},openTab);
+    $(`#settings_btn`).click({tab_name:"settings_tab"},openTab);
+    $(`#profile_btn`).click({tab_name:"profile_tab"},openTab);
+
 });
